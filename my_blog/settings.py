0d69a23dc836616d10a124e50a3f974f53958933
root <zhouxwyeah@163.com>
2017-03-18 12:57:57 +0800

    sync req.txt and setting

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'zh_CN'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -113,3 +113,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
